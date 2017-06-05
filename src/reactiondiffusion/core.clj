d5f4980a616cacea1f91fdd1ac933ba25be8d2ce
Egg Syntax <egg@novonon.com>
2017-06-04 22:01:28 -0400

    Surrounding-ave code, more ascii display

--- src/reactiondiffusion/core.clj
+++ src/reactiondiffusion/core.clj
@@ -26,8 +26,8 @@
 
 ;; Constants
 ;; Display constants
-(def w 8)
-(def h 8)
+(def w 15)
+(def h 15)
 ;; Equation constants
 (def da 1.0)   ; diffusion rate of a
 (def db 0.5)   ; diffusion rate of b
@@ -38,34 +38,65 @@
   "I don't do a whole lot ... yet."
   [& args])
 
-(defn init []
+(defn init-rand []
   (->> (x/zero-matrix h w)
        (x/emap (fn [_] (rand)))))
 
-(defn display-ascii [m]
-  (x/pm (x/emap (fn [v] (if (> v 0.5) "#" " "))
-                (x/coerce [] m))))
+(defn init-middle []
+  (let [x-mid (float (/ w 2))
+        y-mid (float (/ h 2))]
+    (->> (x/zero-matrix h w)
+         (x/emap-indexed (fn [[i j] v]
+                           ;; (println "[i, j]" i j)
+                           (let [x-diff (* -1 (Math/abs (- x-mid i)))
+                                 y-diff (* -1 (Math/abs (- y-mid j)))
+                                 x-val  (/ (+ x-mid x-diff) x-mid)
+                                 y-val  (/ (+ y-mid y-diff) y-mid)]
+                             (* x-val y-val)))))))
 
+(def init init-middle)
+
+(defn display-ascii [m]
+  (x/pm (x/emap (fn [v] (condp <= v ; note: counterintuitively `<` because pred is called as (pred test-val v)
+                         0.66 "#"
+                         0.33 "+"
+                         0.00 " ")
+                  #_(if (> v 0.5) "#" " "))
+                m)))
+
+(defn display-num [m]
+  (x/pm (x/emap (fn [v] (format "%.1f" v))
+                m)))
+
+;; Begin: surrounding-average code
 (defn capped-val [v cap]
-  ;; we have to subtract 2 from cap: 1 because we 0-index and 1 because
-  ;; we don't want them right on the borders
-  (min (max v 1) (- cap 2)))
+  (min (max v 0) (- cap 1)))
 
 (defn index-ranges
   "Return [start length] pairs for x and y, limited to not go out-of-bounds"
   ;; Necessary because submatrix throws exception if any of submatrix is out-of-bounds.
   ;; Would be much nicer for my purposes if submatrix just got a capped submatrix.
   [x y]
-  (assert (and (> x 0) (< x (- w 1)) (> y 0) (< y (- h 1))))
+  ;; (assert (and (> x 0) (< x (- w 1)) (> y 0) (< y (- h 1))))
   (let [x-start (capped-val (- x 1) w)
         x-end   (capped-val (+ x 1) w)
         x-len   (- x-end x-start -1)
         y-start (capped-val (- y 1) h)
         y-end   (capped-val (+ y 1) h)
         y-len   (- y-end y-start -1)]
-    [x-start x-len y-start y-len]))
+    [y-start y-len x-start x-len])) ; reversed because mat ops in y x order
 
 (defn surrounding-vals
   "Get the submatrix immediately surrounding the current position"
   [m x y]
   (apply x/submatrix m (index-ranges x y)))
+
+(defn surrounding-ave
+  "Get the average value surrounding a particular cell"
+  [m x y]
+  (let [curval ((m y) x)
+        neighborhood (surrounding-vals m x y)
+        area-sum (x/esum neighborhood)]
+    (/ (- area-sum curval) (- (x/ecount neighborhood) 1))))
+
+;; End:   surrounding-average code
