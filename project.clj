12dc5db5a9002b6a3083a19166426e0802393974
Egg Syntax <egg@novonon.com>
2017-06-10 15:15:14 -0400

    Add core.async, tools.trace deps

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
+                 [org.clojure/core.async "0.3.443"]
+                 [org.clojure/tools.trace "0.7.9"]
                  [net.mikera/core.matrix "0.59.0"]
                  ;; [bocko "1.0.0"] ; simple graphics
                  [quil "2.6.0"]]
