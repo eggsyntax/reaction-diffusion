0b22fd854c9b0924b9336aac08b857fff1122864
Egg Syntax <egg@novonon.com>
2017-06-10 15:17:44 -0400

    Work toward displaying actual matrix

--- src/reactiondiffusion/display.clj
+++ src/reactiondiffusion/display.clj
@@ -1,26 +1,52 @@
 (ns reactiondiffusion.display
-  (:require [quil.core :as q]))
+  (:require [quil.core :as q]
+            [clojure.core.async :as a :refer [<! go]]
+            [clojure.core.matrix :as x]
+            [reactiondiffusion.core :as core]))
 
 (def w 200)
 (def h 100)
 
+(def state (atom (core/init-middle))) ; updated by core
+
 (defn setup []
-  (q/frame-rate 50)
+  (q/frame-rate 5)
   (q/background 30))
 
+;; (defn draw-cell [m x y]
+;;   (let [v ((m y) x)]
+;;     (q/fill (* v 255) 30 50)
+;;     (q/rect x y 1 1)))
+
+;; TODO not used yet
+;; Assume that draw-cell will be called from x/map-indexed
+(defn draw-cell [[y x] v]
+  ;; (println x y v)
+  (q/fill (* v 255) 30 50)
+  (q/rect x y 1 1))
+
+;; TODO draw can't (I think!) take params.
+;; Is there a way to? Assuming not, could:
+;; - Create an atom for m which is updated by core/next-state
+;; - Could pass in current state map all through the fns in this ns
+;; - Global vars to hold everything?
 (defn draw []
-  (q/stroke (q/random 255))
-  (q/fill (q/random 255))
+  ;; (q/stroke (q/random 255))
+
   (let [diam (q/random 5)
         x (q/random w)
         y (q/random h)]
-    (q/ellipse x y diam  diam)))
+    (println "drawing.")
+    (x/emap-indexed draw-cell @state)
+    (reset! state (core/next-state @state))
+
+    ))
 
 (defn run []
   (q/defsketch example
+    :size [w h]
     :title "foobar"
     :settings #(q/smooth 2)
     :renderer :opengl
     :setup setup
-    :draw draw
-    :size [w h]))
+    :draw draw))
